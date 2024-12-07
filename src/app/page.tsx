f5e9b049074af824f2258ecead1c9ccb138a01e2
ask-nugey <yuki.iwase@blueish.co.jp>
2024-12-08 00:09:08 +0900

    feat: キャッシュの検証

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,152 @@
-import Image from "next/image";
+import { headers } from 'next/headers';
+import { revalidatePath, revalidateTag } from 'next/cache';
+import { Button } from './btn';
 
-export default function Home() {
+type Resp = { dateTime: string };
+
+const typedFetch = (url: string, options?: RequestInit): Promise<Resp> =>
+  fetch(url, options).then((res) => res.json() as Promise<Resp>);
+
+const getData = (): Promise<[Resp, Resp, Resp, Resp, Resp]> =>
+  Promise.all([
+    typedFetch("https://timeapi.io/api/Time/current/zone?timeZone=Europe/Amsterdam", {
+      cache: 'force-cache',
+      next: { tags: ['force-cache', 'all'] },
+    }),
+    typedFetch("https://timeapi.io/api/Time/current/zone?timeZone=Europe/Paris", {
+      cache: 'no-store',
+      next: { tags: ['no-store', 'all'] },
+    }),
+    typedFetch("https://timeapi.io/api/Time/current/zone?timeZone=Europe/Brussels", {
+      next: { revalidate: 10, tags: ['revalidate10', 'revalidate', 'all'] },
+    }),
+    typedFetch("https://timeapi.io/api/Time/current/zone?timeZone=Europe/Paris", {
+      next: { revalidate: 30, tags: ['revalidate30', 'revalidate', 'all'] },
+    }),
+    typedFetch("https://timeapi.io/api/Time/current/zone?timeZone=Europe/London", {
+      next: { revalidate: 60, tags: ['revalidate60', 'revalidate', 'all'] },
+    }),
+  ]);
+
+const revalidateAction = async (tag: string) => {
+  'use server';
+
+  if (tag.startsWith('/')) {
+    revalidatePath(tag);
+  } else {
+    revalidateTag(tag);
+  }
+};
+
+export default async function Home() {
+  const [
+    staticTime,
+    dynamicTime,
+    revalidateTime10,
+    revalidateTime30,
+    revalidateTime60,
+  ] = await getData();
+
+  const headersList = headers();
+  const country = headersList.get('CF-IPCountry') || '???';
+  const city = headersList.get('CF-IPCity') || '???';
+  const region = headersList.get('CF-IPRegion') || '???';
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-50">
+      <main className="flex flex-col items-center flex-1 px-4 sm:px-20 text-center z-10 pt-8 sm:pt-20">
+        <h1 className="text-3xl sm:text-5xl font-bold">
+          Geolocation & Data Cache
+        </h1>
+
+        <p className="mt-4 text-lg sm:text-xl text-gray-700">
+          Show localized content based on headers and show API data cached
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <section className="border max-w-xl mx-auto w-full border-gray-300 bg-white rounded-lg shadow-lg mt-16 hover:shadow-2xl transition">
+          <div className="p-4 flexborder-b bg-gray-50 rounded-b-lg">
+            <h4 className="font-semibold text-left">Geolocation Headers</h4>
+            <pre className="bg-black text-white font-mono text-left py-2 px-4 rounded-lg mt-4 text-sm leading-6">
+              <p>
+                <strong>{'x-vercel-ip-city: '}</strong>
+                {city}
+              </p>
+              <p>
+                <strong>{'x-vercel-ip-country-region: '}</strong>
+                {region}
+              </p>
+              <p>
+                <strong>{'x-vercel-ip-country: '}</strong>
+                {country}
+              </p>
+            </pre>
+          </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div className="p-4 flex justify-center items-between border-b bg-gray-50">
+            <h4 className="font-semibold text-left mr-auto">
+              Static Time (force-cache)
+            </h4>
+            <div className="self-center">
+              <p className="text-gray-700">{staticTime.dateTime}</p>
+            </div>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div className="p-4 flex justify-center items-between border-b bg-gray-50">
+            <h4 className="font-semibold text-left mr-auto">
+              Dynamic Time (no-store)
+            </h4>
+            <div className="self-center">
+              <p className="text-gray-700">{dynamicTime.dateTime}</p>
+            </div>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <div className="p-4 flex justify-center items-between border-b bg-gray-50">
+            <h4 className="font-semibold text-left mr-auto">
+              Dynamic Time <br />
+              (tags revalidate10 & revalidate)
+            </h4>
+            <div className="self-center">
+              <p className="text-gray-700">{revalidateTime10.dateTime}</p>
+            </div>
+          </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <div className="p-4 flex justify-center items-between border-b bg-gray-50">
+            <h4 className="font-semibold text-left mr-auto">
+              Dynamic Time <br />
+              (tags revalidate30 & revalidate)
+            </h4>
+            <div className="self-center">
+              <p className="text-gray-700">{revalidateTime30.dateTime}</p>
+            </div>
+          </div>
+
+          <div className="p-4 flex justify-center items-between border-b bg-gray-50">
+            <h4 className="font-semibold text-left mr-auto">
+              Dynamic Time <br />
+              (tags revalidate60 & revalidate)
+            </h4>
+            <div className="self-center">
+              <p className="text-gray-700">{revalidateTime60.dateTime}</p>
+            </div>
+          </div>
+
+          <div className="p-4 grid grid-cols-3 gap-2 border-b bg-gray-50">
+            <h4 className="font-semibold col-span-3">Revalidate Tags</h4>
+            {[
+              'no-cache',
+              'force-cache',
+              'revalidate',
+              'revalidate10',
+              'revalidate30',
+              'revalidate60',
+              'all',
+              '/',
+            ].map((tag) => (
+              <Button key={tag} action={revalidateAction} tag={tag} />
+            ))}
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
